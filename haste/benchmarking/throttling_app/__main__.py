28a099846d1c7e419faf7cdf0d810ed40c0d3474
Ben Blamey <blamey.ben@gmail.com>
2018-04-18 09:49:42 +0200

    convert to seconds

--- haste/benchmarking/throttling_app/__main__.py
+++ haste/benchmarking/throttling_app/__main__.py
@@ -2,6 +2,7 @@ import time
 from haste.benchmarking.throttling_app.monitor_spark_driver import SparkMonitor
 from sortedcontainers import SortedDict
 
+FETCH_STATUS_INTERVAL_SECONDS = 5
 
 HOST = 'localhost'
 
@@ -12,7 +13,7 @@ monitor.start()
 
 # TODO: hmmm... includes the scheduling delay - what if it simply gets behind. get it errs on caution.
 # key[0] is the *oldest* key[-1] is the *newest*
-total_delays_by_timestamp = SortedDict()
+total_delays_secs_by_timestamp = SortedDict()
 
 while True:
 
@@ -26,17 +27,17 @@ while True:
             print('..skipping batch ' + batch['batchId'] + ' with status ' + batch['status'])
 
         timestamp = batch['batchTime']
-        total_delay = batch['totalDelay']
+        total_delay = int(batch['totalDelay'])/1000
 
-        total_delays_by_timestamp[timestamp] = total_delay
+        total_delays_secs_by_timestamp[timestamp] = total_delay
 
 
 
-    print(total_delays_by_timestamp.keys()[-1])
+    print(total_delays_secs_by_timestamp.items()[-1])
 
     # avgTotalDelay = stats['avgTotalDelay']
     #
     # percent_of_batch_interval = int((avgTotalDelay / 1000) * 100)
 
 
-    time.sleep(5)
+    time.sleep(FETCH_STATUS_INTERVAL_SECONDS)
